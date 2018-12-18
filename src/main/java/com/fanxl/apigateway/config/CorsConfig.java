2d74e211bc98ec1abeded5f47cfecc09b7215955
fanxl12 <744822514@qq.com>
2018-12-18 20:33:04 +0800

    增加跨域处理;

---
+++ src/main/java/com/fanxl/apigateway/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.fanxl.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
+/**
+ * @description 跨域配置
+ * @author: fanxl
+ * @date: 2018/12/18 0018 20:25
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setMaxAge(300L);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+
+}
